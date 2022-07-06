26f5d8a9980171d2f821879032a903c9c039ff6f
ramsrimathi <103913090+ramsrimathi@users.noreply.github.com>
2022-07-06 11:04:59 -0700

    Update tomcatdev.groovy

--- vars/tomcatdev.groovy
+++ vars/tomcatdev.groovy
@@ -1,9 +1,11 @@
-def call(credID,userName,ipAddr){
-   sshagent ([credID]) {
-        sh "mv target/*.war target/webapp.war"
+def call(credId,userName,ipAddr){ 
+   sshagent ([credId]) {
+     sh "mv target/*.war target/webapp.war"
      sh "scp -o StrictHostKeyChecking=no target/webapp.war ${userName}@${ipAddr}:/opt/tomcat9/webapps"
      sh "ssh ${userName}@${ipAddr} /opt/tomcat9/bin/shutdown.sh"
      sh "ssh ${userName}@${ipAddr} /opt/tomcat9/bin/startup.sh"
    }
 }
-}
+
+   
+   
